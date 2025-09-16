3320fd084e0c36f486ec56d9272859269bb04733
ppregin-byte <ppregin@gmail.com>
2025-09-16 14:22:39 +0200

    описание того, что сделал

---
+++ session.py
@@ -0,0 +1,9 @@
+has_degree = True
+Sreaks_english = True
+Has_drivers_lisense = False
+is_certisied = Falseyears_of_experience = 5
+
+print (has degree)
+
+if has_degree and years_of_experience > 4 
+print(@)
